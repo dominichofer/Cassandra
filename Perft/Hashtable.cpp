0bff131d62f02a73fe910d0b26af37386d28c14d
Dominic Hofer <dohofer86@gmail.com>
2021-11-29 18:49:55 +0100

    Maktes use of CTAD.

--- Perft/Hashtable.cpp
+++ Perft/Hashtable.cpp
@@ -2,11 +2,11 @@
 
 void BigNode::Update(const key_type& new_key, const value_type& new_value)
 {
-	ScopedLock<decltype(m_value)> lock_guard{m_value};
-	if (new_value > lock_guard.value)
+	ScopedLock lock{m_value};
+	if (new_value > lock.value)
 	{
 		m_key = new_key;
-		lock_guard.value = new_value;
+		lock.value = new_value;
 	}
 }
 
@@ -20,13 +20,13 @@ std::optional<BigNode::value_type> BigNode::LookUp(const key_type& key) const
 
 void BigNode::Clear()
 {
-	ScopedLock<decltype(m_value)> lock_guard{m_value};
+	ScopedLock lock{m_value};
 	m_key = key_type{};
-	lock_guard.value = 0;
+	lock.value = 0;
 }
 
 BigNode::KeyValuePair BigNode::Get() const
 {
-	ScopedLock<decltype(m_value)> lock_guard{m_value};
-	return { m_key, lock_guard.value };
+	ScopedLock lock{m_value};
+	return { m_key, lock.value };
 }
